3a4159f11414c638b02a79cd86fed2c7dc599d09
Julie VALENTIN <62503654+julieVal@users.noreply.github.com>
2020-10-27 13:37:22 +0100

    conserver les valeur après soubmit

--- index.php
+++ index.php
@@ -3,20 +3,30 @@ require_once("modele/modele.inc.php");
 session_start();
 
 // Initialisation des variables
-
 $action = 'calculatrice';
-/*
-echo'Session ';var_dump ($_SESSION);
-echo'Post ';var_dump($_POST);
-echo'GET ';var_dump($_GET);
-echo'files ';var_dump($_FILES);
-echo "userID : ".$userID.'</br>';
-*/
+if(isset($_GET['action'])){
+    $action = $_GET['action'];
+}
+
+
+
+
+echo "Post <br>";
+var_dump($_POST);
+echo "Get <br>";
+var_dump($_GET);
 // Etapes et traitements
 switch ($action) {
     case 'calculatrice':
-    
+        require('vues/view_header.php');
+        require('vues/view_calculatrice.php');
+        require('vues/view_footer.php');
+    break;
+    case 'calculer':
+        
+        require('vues/view_header.php');
+        require('vues/view_calculatrice.php');
+        require('vues/view_footer.php');
+    break;
+
 }
-require('vues/view_header.php');
-require('vues/view_calculatrice.php');
-require('vues/view_footer.php');
--- vues/view_calculatrice.php
+++ vues/view_calculatrice.php
@@ -1,21 +1,30 @@
 <?php
-
+if(isset($_POST['nbr1']) && isset($_POST['nbr2']) && isset($_POST['operator'])){
+    $nbr1 = $_POST['nbr1'];
+    $nbr2 = $_POST['nbr2'];
+    $operator = $_POST['operator'];
+}
+else{
+    $nbr1 = null;
+    $nbr2 = null;
+    $operator = null;
+}
 ?>
 <div class="container-fluid">
 <h1>Ceci est une calculatrice</h1>
 
     <form action="<?php echo $_SERVER['PHP_SELF'].'?action=calculer'?>" method="post" class="form-group col-md-3 mx-auto">
     
-        <input class="form-control mb-2" type="text" name="nbr1" id="nbr1" pattern="^[-]?[0-9]+[.]?[0-9]+" placeholder="Entrez un nombre" required>
+        <input class="form-control mb-2" type="text" name="nbr1" id="nbr1" pattern="^[-]?[0-9]+[.]?[0-9]+" placeholder="Entrez un nombre" required value="<?php echo $nbr1; ?>">
 
         <select class="form-control mb-2"  name="operator" id="operator">
-            <option value="addition">+</option>
-            <option value="soustraction">-</option>
-            <option value="multiplication">*</option>
-            <option value="division">/</option>
+            <option value="addition" <?php if ($operator == 'addition') echo "selected"; ?> >+</option>
+            <option value="soustraction" <?php if ($operator == 'soustraction') echo "selected"; ?>>-</option>
+            <option value="multiplication" <?php if ($operator == 'multiplication') echo "selected"; ?>>*</option>
+            <option value="division" <?php if ($operator == 'division') echo "selected"; ?>>/</option>
         </select>
 
-        <input class="form-control mb-2" type="text" name="nbr2" id="nbr2" pattern="^[-]?[0-9]+[.]?[0-9]+" placeholder="Entrez un nombre" required>
+        <input class="form-control mb-2" type="text" name="nbr2" id="nbr2" pattern="^[-]?[0-9]+[.]?[0-9]+" placeholder="Entrez un nombre" required value="<?php echo $nbr2; ?>">
 
         <input class="btn btn-danger" type="submit" value="Caluler">
        
