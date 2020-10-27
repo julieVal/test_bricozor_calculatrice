2032bc89b38c02dd36c8bfa7836dd1b63cae9786
Julie VALENTIN <62503654+julieVal@users.noreply.github.com>
2020-10-27 15:25:11 +0100

    css historique

--- vues/view_historique.php
+++ vues/view_historique.php
@@ -1,13 +1,13 @@
-<div class="container-fluid">
-    <div class="col-md-6 mx-auto">
-    <h1>Historique des résultats</h1>
-        <?php echo $historique; ?>
-
-        <form action="<?php echo $_SERVER['PHP_SELF'].'?action=calculatrice'?>" method="post" class="form-group">
-            <input class ="btn btn-info" type="submit" value="Retour à la caluclatrice">
-        </form>
+<div class="bg-secondary w-100" style="height: 100vh;">
+    <div class="container-fluid pt-5">
+        <div class="col-md-6 mx-auto">
+            <h1>Historique des résultats</h1>
+            <div class="bg-light text-dark rounded">
+                <p class="pl-2 pt-1"><?php echo $historique; ?></p>
+            </div>
+                <form action="<?php echo $_SERVER['PHP_SELF'].'?action=calculatrice'?>" method="post" class="form-group">
+                    <input class ="btn btn-info" type="submit" value="Retour à la calculatrice">
+                </form>
+        </div>
     </div>
-
-
-
 </div>
