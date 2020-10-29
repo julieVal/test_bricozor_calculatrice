dc6382bf5964ecda159423bc346c0ef536a75a7f
Julie VALENTIN <62503654+julieVal@users.noreply.github.com>
2020-10-29 12:24:21 +0100

    btn enregistrer

--- vues/view_calculatrice.php
+++ vues/view_calculatrice.php
@@ -12,14 +12,22 @@
         <!--  visuel calculatrice -->
         <div class="calculatrice col-lg-8 col-sm-10 mx-auto">
             <!-- Zone d'affichage-->
-            <div id="en_cour" class="en_cour col-10 mx-auto text-light text-right" style="height: 50px";><?php echo $tabCalcul ?></div>
+            <div id="en_cours" class="col-10 mx-auto text-light text-right";><?php echo $tabCalcul ?></div>
             <div class="row">
                 <div class="calcul bg-light col-10 mx-auto text-right font-weight-bold" id="calcul" style="height: 50px"; value="<?php echo $resultat ?>"> <?php echo $resultat ?></div>
                
             </div>
             <!-- Pavé numérique-->
-            <div class="row mt-2">
+            <div class="action row mt-2 mb-1">
                 <input type="button" class="btn btn-warning col-6" id="delete" value="Annuler">
+                <!-- enregistrement du résultat -->
+                <div id="save" class="col-6">
+                    <form action="<?php echo $_SERVER['PHP_SELF'].'?action=saveHistory'?>" method="post">
+                        <input class="btn btn-info btn-block" id="inputSave" type="submit" value="Enregistrer">
+                        <input type="hidden" name="resultat" id="resultat" value="">
+                        <input type="hidden" name="tabCalcul" id="tabCalcul" value="">
+                    </form>
+                </div>
             </div>
             <div class="pave row mt-1">
                 <input type="button" class="btn btn-light col-3" id="7" value="7">
@@ -47,14 +55,7 @@
             </div>
         </div>
 
-        <!-- enregistrement du résultat -->
-        <div id="save" class="d-none">
-            <form action="<?php echo $_SERVER['PHP_SELF'].'?action=saveHistory'?>" method="post" class="form-group">
-                <input class="btn btn-info" type="submit" value="Enregistrer le résultat">
-                <input type="hidden" name="resultat" id="resultat" value="">
-                <input type="hidden" name="tabCalcul" id="tabCalcul" value="">
-            </form>
-        </div>
+        
         
         <!-- formulaire d'accès à l'historique -->
         <div class="col-lg-8 mt-3 text-center mx-auto">
