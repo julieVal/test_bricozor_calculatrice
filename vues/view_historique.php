35a8de466d6dce35f136788977c38b2542e878a2
Julie VALENTIN <62503654+julieVal@users.noreply.github.com>
2020-10-29 11:58:57 +0100

    css historique

--- vues/view_historique.php
+++ vues/view_historique.php
@@ -1,15 +1,15 @@
-<div class="bg-secondary w-100" style="height: 100vh;">
+<div class="history bg-secondary w-100">
     <div class="container-fluid pt-5">
         <div class="col-md-6 mx-auto">
             <h1>Historique des résultats</h1>
             <!-- affichage de l'historique -->
-            <div class="history bg-light text-dark rounded">
+            <div class="historyList bg-light text-dark rounded">
                 <p class="pl-2 pt-1"><?php echo $historique; ?></p>
             </div>
 
             <!-- Retour vers la calculatrice vide -->
             <form action="<?php echo $_SERVER['PHP_SELF'].'?action=calculatrice'?>" method="post" class="form-group">
-                <input class ="btn btn-info" type="submit" value="Retour à la calculatrice">
+                <input class ="btn btn-info mt-2" type="submit" value="Retour à la calculatrice">
             </form>
         </div>
     </div>
